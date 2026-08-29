8039098cc788bbb1feda24b7c6bd4d9be8bc0f89
Karol Rincon <kgsarmientor@itc.edu.co>
2026-08-29 11:17:05 -0500

    feat: implementacion spring mvc y thymeleaf semana 4

---
+++ Unilex/src/main/java/com/unilex/app/Unilex/Controller/ProcesoViewController.java
@@ -0,0 +1,30 @@
+package com.unilex.app.Unilex.Controller;
+
+import com.unilex.app.Unilex. Service.ProcesoJudicialService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProcesoViewController {
+
+    private final ProcesoJudicialService procesoService;
+
+    // Inyección de dependencias por constructor
+    public ProcesoViewController(ProcesoJudicialService procesoService) {
+        this.procesoService = procesoService;
+    }
+
+    @GetMapping("/procesos")
+    public String verListaProcesos(Model model) {
+        // Obtenemos los datos desde el servicio
+        var listaProcesos = procesoService.listarProcesos();
+
+        // Enviamos la lista a la vista mediante Model
+        model.addAttribute("procesos", listaProcesos);
+        model.addAttribute("titulo", "Unilex - Sistema de Gestión de Procesos Judiciales");
+
+        // Retorna el nombre de la plantilla HTML (procesos.html en templates)
+        return "procesos";
+    }
+}
--- Unilex/src/main/java/com/unilex/app/Unilex/Model/ProcesoJudicial.java
+++ Unilex/src/main/java/com/unilex/app/Unilex/Model/ProcesoJudicial.java
@@ -4,16 +4,28 @@ public class ProcesoJudicial {
     private Long id;
     private String radicado;
     private String juzgado;
+    private String tipoProceso;
+    private String estado;
 
     // Constructor
     public ProcesoJudicial(Long id, String radicado, String juzgado) {
         this.id = id;
         this.radicado = radicado;
         this.juzgado = juzgado;
+        this.tipoProceso = tipoProceso;
+        this.estado = estado;
     }
 
     // Métodos Get y Set (Requeridos para el POJO)
     public Long getId() { return id; }
     public String getRadicado() { return radicado; }
     public String getJuzgado() { return juzgado; }
+    public String getTipoProceso() { return tipoProceso; }
+    public String getEstado() { return estado; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setRadicado(String radicado) { this.radicado = radicado; }
+    public void setJuzgado(String juzgado) {this.juzgado = juzgado; }
+    public void setTipoProceso(String tipoProceso) { this.tipoProceso = tipoProceso; }
+    public void setEstado(String estado) { this.estado = estado; }
 }
